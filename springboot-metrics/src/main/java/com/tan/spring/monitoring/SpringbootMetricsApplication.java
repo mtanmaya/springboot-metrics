26cc3c423c0b2e73e3250348b2542deab02be1bc
tmalik <tmalik@localhost>
2020-04-20 01:40:10 -0400

    -- initial setup -- added micrometer -- added prometheus

---
+++ springboot-metrics/src/main/java/com/tan/spring/monitoring/SpringbootMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.tan.spring.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMetricsApplication.class, args);
+	}
+
+}
---
+++ springboot-metrics/src/test/java/com/tan/spring/monitoring/SpringbootMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tan.spring.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
